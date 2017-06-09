8eec04fd894a1f8338bb1757702f79ce120177db
tomik3690 <tomik031195@gmail.com>
2017-06-09 14:31:09 +0200

    confirm back button on game state

--- src/app/appRun.js
+++ src/app/appRun.js
@@ -1,8 +1,24 @@
-export default function ($rootScope) {
+export default function ($rootScope, $state) {
     $rootScope.directions = { 
         upRight: 'UP_RIGHT',
         upLeft: 'UP_LEFT',
         downRight: 'DOWN_RIGHT', 
         downLeft: 'DOWN_LEFT'
     };
+
+    document.addEventListener('deviceready', () => {
+        document.addEventListener('backbutton', () => {
+            switch ($state.current.name) {
+                case 'game': {
+                    let goBack = confirm('Are you sure?');
+                    
+                    if (goBack) {
+                        $state.go('home');
+                    }
+
+                    break;
+                }
+            }
+        });
+    });
 }
